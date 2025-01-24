7db4b4cb23251f8c53dcebee6b5f9634cb3d35cc
TC318 <tyler.cooper318@gmail.com>
2025-01-24 16:26:38 +0900

    Added navbar functionality

--- Daikon/Daikon/urls.py
+++ Daikon/Daikon/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("home.urls")),
+    path("sortingalgs/", include("SortingAlgs.urls")),
     path('admin/', admin.site.urls),
 ]
